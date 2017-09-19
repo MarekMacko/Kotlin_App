222d211b5c91d0f6caee429172b5188d4b8b4fc9
Marek Macko <marek.macko@intive.com>
2017-09-19 14:29:41 +0200

    Return disposable from retrofit fetching

--- app/src/main/java/com/marekmacko/kotlinapp/mvp/WeatherPresenter.kt
+++ app/src/main/java/com/marekmacko/kotlinapp/mvp/WeatherPresenter.kt
@@ -1,16 +1,20 @@
 package com.marekmacko.kotlinapp.mvp
 
 import com.marekmacko.kotlinapp.repository.WeatherRepository
+import io.reactivex.disposables.CompositeDisposable
 import javax.inject.Inject
 
 
 class WeatherPresenter @Inject constructor(private val view: WeatherMvp.View,
                                            private val weatherRepository: WeatherRepository)
     : WeatherMvp.Presenter {
+    private val compositeDisposable = CompositeDisposable()
 
-    override fun fetchForecast() = // TODO: add disposable
-            weatherRepository.getWeeklyForecast(
-                    { view.updateWeeklyForecast(it) },
-                    { view.showError(it) }
-            )
+    override fun fetchForecast() {
+        val disposable = weatherRepository.getWeeklyForecast(
+                { view.updateWeeklyForecast(it) },
+                { view.showError(it) }
+        )
+        compositeDisposable.add(disposable)
+    }
 }
--- app/src/main/java/com/marekmacko/kotlinapp/repository/RemoteWeatherRepository.kt
+++ app/src/main/java/com/marekmacko/kotlinapp/repository/RemoteWeatherRepository.kt
@@ -3,6 +3,8 @@ package com.marekmacko.kotlinapp.repository
 import com.marekmacko.kotlinapp.api.WeatherService
 import com.marekmacko.kotlinapp.data.WeeklyForecast
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 
 
@@ -12,13 +14,12 @@ class RemoteWeatherRepository(private val weatherService: WeatherService) {
         private const val API_ZIP_CODE = "94043"
     }
 
-    fun getWeeklyForecast(onLoad: (WeeklyForecast) -> Unit, onError: (String) -> Unit) {
-        weatherService.getWeeklyForecast(API_ZIP_CODE)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        { onLoad(it) },
-                        { onError(it.message ?: "No message provided") }
-                )
-    }
+    fun getWeeklyForecast(onLoad: (WeeklyForecast) -> Unit, onError: (String) -> Unit): Disposable =
+            weatherService.getWeeklyForecast(API_ZIP_CODE)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(
+                            { onLoad(it) },
+                            { onError(it.message ?: "No message provided") }
+                    )
 }
