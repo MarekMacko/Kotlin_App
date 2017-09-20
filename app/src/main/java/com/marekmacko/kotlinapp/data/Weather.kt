ca7ec5010cd4ce9eeb586b6650b99f4a89e37626
Marek Macko <marek.macko@intive.com>
2017-09-20 10:52:36 +0200

    Add missing Serializable to data models

--- app/src/main/java/com/marekmacko/kotlinapp/data/Weather.kt
+++ app/src/main/java/com/marekmacko/kotlinapp/data/Weather.kt
@@ -1,9 +1,12 @@
 package com.marekmacko.kotlinapp.data
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 data class Weather(val id: Long, val main: String,
-                   val description: String, @SerializedName("icon") private val iconCode: String) {
+                   val description: String, @SerializedName("icon") private val iconCode: String)
+    : Serializable {
+
     val iconUrl: String
         get() = "http://openweathermap.org/img/w/$iconCode.png"
 }
