035cd7413eac8401294cf38a48610f2648c7fc5b
Marek Macko <marek.macko@intive.com>
2017-09-21 10:10:14 +0200

    Fix adapter possibles null pointers

--- app/src/main/java/com/marekmacko/kotlinapp/ForecastListAdapter.kt
+++ app/src/main/java/com/marekmacko/kotlinapp/ForecastListAdapter.kt
@@ -5,13 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.marekmacko.kotlinapp.data.DailyForecast
+import com.marekmacko.kotlinapp.data.Temperature
+import com.marekmacko.kotlinapp.data.Weather
 import com.marekmacko.kotlinapp.data.WeeklyForecast
 import com.marekmacko.kotlinapp.util.loadFromUrl
 import kotlinx.android.synthetic.main.item_forecast.view.*
 
-class ForecastListAdapter(private var weeklyForecast: WeeklyForecast?,
-                          private val itemClick: (DailyForecast) -> Unit)
+class ForecastListAdapter(private val itemClick: (DailyForecast) -> Unit)
     : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    private var weeklyForecast: WeeklyForecast? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -22,9 +25,9 @@ class ForecastListAdapter(private var weeklyForecast: WeeklyForecast?,
     override fun onBindViewHolder(holder: ViewHolder, position: Int) =
             holder.bindDailyForecast(weeklyForecast!![position])
 
-    override fun getItemCount(): Int = weeklyForecast?.size?: 0
+    override fun getItemCount(): Int = weeklyForecast?.size ?: 0
 
-    fun setItemsAndNotify(weeklyForecast: WeeklyForecast) {
+    fun setItems(weeklyForecast: WeeklyForecast) {
         this.weeklyForecast = weeklyForecast
         notifyDataSetChanged()
     }
@@ -33,13 +36,27 @@ class ForecastListAdapter(private var weeklyForecast: WeeklyForecast?,
         : RecyclerView.ViewHolder(view) {
 
         fun bindDailyForecast(dailyForecast: DailyForecast) = with(dailyForecast) {
-            val weather = weather[0]
-            itemView.icon.loadFromUrl(weather.iconUrl)
+            bindWeather(weather)
+            bindTemperature(temp)
             itemView.date.text = date
-            itemView.description.text = weather.description
-            itemView.maxTemperature.text = "${temp.max}"
-            itemView.minTemperature.text = "${temp.min}"
             itemView.setOnClickListener { itemClick(this) }
+        }
+
+        private fun bindWeather(weather: List<Weather>?) {
+            if (weather == null || weather.isEmpty()) {
+                return
+            }
+            with(weather[0]) {
+                itemView.icon.loadFromUrl(iconUrl)
+                itemView.description.text = description
+            }
+        }
+
+        private fun bindTemperature(temperature: Temperature?) {
+            temperature?.apply {
+                itemView.maxTemperature.text = "$max"
+                itemView.minTemperature.text = "$min"
+            }
         }
     }
 }
