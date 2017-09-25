14929d6cb72eeaacfe71d767b55477ba4df4e873
Marek Macko <marek.macko@intive.com>
2017-09-25 15:30:33 +0200

    Remove unnecessary RepositoryModule

--- app/src/main/java/com/marekmacko/kotlinapp/WeatherComponent.kt
+++ app/src/main/java/com/marekmacko/kotlinapp/WeatherComponent.kt
@@ -2,15 +2,12 @@ package com.marekmacko.kotlinapp
 
 import com.marekmacko.kotlinapp.api.NetworkModule
 import com.marekmacko.kotlinapp.mvp.WeatherPresenterModule
-import com.marekmacko.kotlinapp.repository.RepositoryModule
 import com.marekmacko.kotlinapp.ui.WeeklyForecastFragment
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = arrayOf(NetworkModule::class, RepositoryModule::class,
-        WeatherPresenterModule::class)
-)
+@Component(modules = arrayOf(NetworkModule::class, WeatherPresenterModule::class))
 interface WeatherComponent {
 
     fun inject(weeklyForecastFragment: WeeklyForecastFragment)
